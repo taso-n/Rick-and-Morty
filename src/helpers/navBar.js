9a7632ddede89deddf05ee4a27da0bdbae54f808
taso-n <aniko17@freeuni.edu.ge>
2021-02-14 21:26:36 +0400

    add scroll to top feature

--- src/helpers/navBar.js
+++ src/helpers/navBar.js
@@ -11,4 +11,16 @@ document.addEventListener('click', function (e) {
     if(e.target.className === 'link'){
         closeNavBar()
     }
+    if(e.target.id === 'scrollToTopButton') {
+        window.scrollTo(0, 0)
+    }
 })
+
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        document.getElementById("scrollToTop").style.display = 'block'
+    } else {
+        document.getElementById("scrollToTop").style.display = 'none'
+    }
+};
